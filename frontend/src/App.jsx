d9633085dbc54a83fd3907428bbd077fc33063b9
Anne Liza Wangui <annewangui210@gmail.com>
2025-11-03 22:44:11 +0300

    axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 //import React from 'react'
 
 import { Routes, Route } from "react-router"
+import { useEffect, useState } from "react"
+import toast, { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
 
 import HomePage from "./pages/HomePage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
@@ -10,13 +15,25 @@ import CallPage from "./pages/CallPage.jsx"
 import ChatPage from "./pages/ChatPage.jsx"
 import OnboardingPage from "./pages/OnboardingPage.jsx"
 
-import toast, { Toaster } from "react-hot-toast"
+
 
 
 const App = () => {
+  // tanstack query
+  const {data,isLoading,error} = useQuery({
+    queryKey: ["todos"],
+
+    queryFn: async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+      return res.data
+    },
+  })
+  console.log({data})
+  console.log({isLoading})
+  console.log({error})
+
   return (
     <div className=" h-screen" data-theme="night">
-    
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,22 @@ import App from './App.jsx'
 
 import { BrowserRouter } from "react-router";
 
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <App />
+    </QueryClientProvider>
     </BrowserRouter>
-
   </StrictMode>,
-)
+);
